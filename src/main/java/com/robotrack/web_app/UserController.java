8b3df1cbdfa75ef5ea1bf5bf22d6b41a8589e3fe
Aleksey_Pahalev <83154713+Whynot46@users.noreply.github.com>
2024-12-07 23:44:42 +0400

    update UserController

--- src/main/java/com/robotrack/web_app/UserController.java
+++ src/main/java/com/robotrack/web_app/UserController.java
@@ -2,13 +2,12 @@ package com.robotrack.web_app;
 
 import java.util.ArrayList;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController{
     private ArrayList<User> users = new ArrayList<User>();
 
